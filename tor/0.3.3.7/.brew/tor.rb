6994d1128c89bb58e693ae874f2e7dc37f76dc25
kyoshiro972 <hafizh@Hafizhs-MBP.mshome.net>
2018-06-13 17:07:01 +0800

    Initial commit

---
+++ tor/0.3.3.7/.brew/tor.rb
@@ -0,0 +1,63 @@
+class Tor < Formula
+  desc "Anonymizing overlay network for TCP"
+  homepage "https://www.torproject.org/"
+  url "https://www.torproject.org/dist/tor-0.3.3.7.tar.gz"
+  mirror "https://tor.eff.org/dist/tor-0.3.3.7.tar.gz"
+  sha256 "ea6bb512c4adfbc4e05b22e4c2d06bddff5b358a53de982273fec846b75bde0c"
+
+  depends_on "pkg-config" => :build
+  depends_on "libevent"
+  depends_on "openssl"
+  depends_on "libscrypt" => :optional
+
+  def install
+    args = %W[
+      --disable-dependency-tracking
+      --disable-silent-rules
+      --prefix=#{prefix}
+      --sysconfdir=#{etc}
+      --localstatedir=#{var}
+      --with-openssl-dir=#{Formula["openssl"].opt_prefix}
+    ]
+
+    args << "--disable-libscrypt" if build.without? "libscrypt"
+
+    system "./configure", *args
+    system "make", "install"
+  end
+
+  plist_options :manual => "tor"
+
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>KeepAlive</key>
+        <true/>
+        <key>ProgramArguments</key>
+        <array>
+            <string>#{opt_bin}/tor</string>
+        </array>
+        <key>WorkingDirectory</key>
+        <string>#{HOMEBREW_PREFIX}</string>
+        <key>StandardErrorPath</key>
+        <string>#{var}/log/tor.log</string>
+        <key>StandardOutPath</key>
+        <string>#{var}/log/tor.log</string>
+      </dict>
+    </plist>
+  EOS
+  end
+
+  test do
+    pipe_output("script -q /dev/null #{bin}/tor-gencert --create-identity-key", "passwd\npasswd\n")
+    assert_predicate testpath/"authority_certificate", :exist?
+    assert_predicate testpath/"authority_signing_key", :exist?
+    assert_predicate testpath/"authority_identity_key", :exist?
+  end
+end
